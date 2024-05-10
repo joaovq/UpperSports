7d1897d1f1ab2ccf85042e73165c4786b40654da
joaovq <joaovqdev@gmail.com>
2024-05-09 21:26:08 -0300

    build: add dependencies for impl firebase in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,4 +139,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
